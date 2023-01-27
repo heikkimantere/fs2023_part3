2246ce10904fca2e9895f27de95f8a4c276b421f
Heikki Mantere <heikki.mantere@gmail.com>
2023-01-27 12:35:46 +0200

    3.13 + 3.14

--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
+
+const formatRequest = (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    JSON.stringify(req.body),
+  ].join(" ");
+};
 
 const app = express();
 app.use(express.json());
 app.use(express.static("build"));
 app.use(cors());
-app.use(
-  morgan((tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      JSON.stringify(req.body),
-    ].join(" ");
-  })
-);
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+app.use(morgan(formatRequest));
 
 app.get("/", (req, res) => {
   res.send("<h1>Here we are!</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -72,30 +49,32 @@ app.post("/api/persons", (req, res) => {
   const body = req.body;
 
   if (!body.name) {
-    return res.status(400).json({ error: "Name is missing" }).end();
+    return res.status(400).json({ error: "Name is missing" }); //.end();
   }
   if (!body.number) {
     return res.status(400).json({ error: "Number is missing" }).end();
   }
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({ error: "Name exists already" }).end();
-  }
-
-  const id = Math.round(Math.random() * 10000);
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id,
-  };
-  persons = persons.concat(person);
-  res.json(person);
+  });
+
+  person.save().then((savedPerson) => {
+    console.log(
+      `added ${savedPerson.name} number ${savedPerson.number} to phonebook`
+    );
+    res.json(savedPerson);
+  });
 });
 
 app.get("/info", (req, res) => {
   const time = new Date();
   const weekday = time.toLocaleString("default", { weekday: "short" });
   const month = time.toLocaleString("default", { month: "short" });
-  res.send(`<div>
+
+  Person.find({}).then((persons) => {
+    console.log(persons.length);
+    res.send(`<div>
   <p>Phonebook has info for ${persons.length} people</p>
   <p>
   ${weekday} 
@@ -104,9 +83,10 @@ app.get("/info", (req, res) => {
   ${time.getFullYear()} 
   ${time.toTimeString()}</p>
   </div>`);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, "0.0.0.0", () => {
+const PORT = process.env.PORT; // || 3001;
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- backend/mongo.js
+++ backend/mongo.js
@@ -1,15 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
-
-const password = process.argv[2];
 const newName = process.argv[3];
 const phonenumber = process.argv[4];
-
-const url = `mongodb+srv://heman:${password}@cluster0.msaghfm.mongodb.net/personsApp?retryWrites=true&w=majority`;
+const url = process.env.MONGODB_URI;
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
